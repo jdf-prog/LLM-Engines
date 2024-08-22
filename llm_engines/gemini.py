d879e00513899261b891886fb3772b3f961ca8cc
Jingjing Li <jl3676@cornell.edu>
2024-08-22 15:43:57 -0700

    gemini fixes

--- llm_engines/gemini.py
+++ llm_engines/gemini.py
@@ -30,11 +30,9 @@ safety_settings = [
 # no image, multi-turn, do not use openai_generate, but can refer to it
 def call_worker_gemini(messages:List[str], model_name, timeout:int=60, conv_system_msg=None, **generate_kwargs) -> str:
     # change messages to gemini format
-    model = genai.GenerativeModel(model_name)
+    model = genai.GenerativeModel(model_name, system_instruction=conv_system_msg)
     
     new_messages = []
-    if conv_system_msg:
-        new_messages.append({"role": "system", "parts": [glm.Part(text=conv_system_msg)]})
     for i, message in enumerate(messages):
         new_messages.append({"role": "user" if i % 2 == 0 else "model", "parts": [glm.Part(text=message)]})
     
@@ -55,7 +53,10 @@ def call_worker_gemini(messages:List[str], model_name, timeout:int=60, conv_syst
     def generate_content():
         return model.generate_content(new_messages, safety_settings=safety_settings, generation_config=generation_config, request_options=request_options)
     response = generate_content()
-    return response.text
+    try:
+        return response.text
+    except:
+        return None
 
 if __name__ == "__main__":
     from icecream import ic
